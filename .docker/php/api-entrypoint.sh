6bcd1788317e3da6306740bafca60852f9d7fb66
Paweł Kęska <projekty@pawelkeska.eu>
2023-11-19 21:34:15 +0100

    [Docker] Exclude filesystems which might not support setfacl

--- .docker/php/api-entrypoint.sh
+++ .docker/php/api-entrypoint.sh
@@ -9,7 +9,10 @@ fi
 
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
 	mkdir -p apps/api/var/cache apps/api/var/log
-	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX apps/api/var || true
+
+	# @note: certain filesystems might not support setfacl
+	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX apps/api/var || true
+ 	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX apps/api/var || true
 
 	if [ ! -e ".env.local" ]; then
 		cp -p .env .env.local
